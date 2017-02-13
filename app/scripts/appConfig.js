2d63c2f5d8ababbfd1bd24b4cf084fc4e40df6b7
Thodoris Greasidis <thgreasi@gmail.com>
2017-02-13 19:55:35 +0200

    feat(appConfig): use cors-anywhere on https

--- app/scripts/appConfig.js
+++ app/scripts/appConfig.js
@@ -1,2 +1,11 @@
-export var OpenWeatherMap_WS_BASE_URL = 'http://api.openweathermap.org/data/2.5/';
+import { location } from 'Window';
+
+export var CORS_SERVICE_URL_PREFIX = 'https://cors-anywhere.herokuapp.com/';
+
+var isHttps = location && location.href.toLowerCase().startsWith('https:');
+var OpenWeatherMap_WS_Prefix = isHttps ?
+	CORS_SERVICE_URL_PREFIX :
+	'';
+
+export var OpenWeatherMap_WS_BASE_URL = OpenWeatherMap_WS_Prefix + 'http://api.openweathermap.org/data/2.5/';
 export var OpenWeatherMap_WS_AppId = '6bd9686d6698729a80ce12d387467d60';
