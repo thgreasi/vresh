0e502431043569a209d7d6da973e479251cc74c2
Thodoris Greasidis <thgreasi@gmail.com>
2017-02-18 20:12:29 +0200

    feat(weather-mock): clone some properies and abstract the code

--- app/scripts/Services/WeatherServiceMock.js
+++ app/scripts/Services/WeatherServiceMock.js
@@ -5,16 +5,29 @@ import CityWeatherDetails from '../Model/CityWeatherDetails';
 
 export class WeatherService {
 
+    static _prepareResult(result) {
+        result = Object.assign(new CityWeatherDetails(), result);
+        Object.keys(result).forEach(key => {
+            if (Array.isArray(result[key])) {
+                result[key] = result[key].slice();
+            } else if (typeof result[key] === 'object') {
+                result[key] = Object.assign({}, result[key]);
+            }
+        });
+        if (result.main) {
+            result.main.temp += (Math.random() >= 0.5 ? 1 : -1) * Math.round(Math.random() * 10);
+        }
+        result.setTemperatures();
+        return result;
+    }
+
     // http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1
     static getCityWeather (cityname) {
         console.log(`Mock request: \${BASE_URL}weather?q=${cityname}&appid=\${appid}`);
         return new Promise(resolve => {
             setTimeout(() => {
-                var result = Object.assign(new CityWeatherDetails(), mockCityData[cityname]);
-                if (result.main) {
-                    result.main.temp += (Math.random() >= 0.5 ? 1 : -1) * Math.round(Math.random() * 5);
-                }
-                result.setTemperatures();
+                var result = mockCityData[cityname];
+                result = WeatherService._prepareResult(result);
                 resolve(result);
             }, 700);
         });
@@ -24,12 +37,8 @@ export class WeatherService {
         console.log(`Mock request: \${BASE_URL}weather?id=${cityID}&appid=\${appid}`);
         return new Promise(resolve => {
             setTimeout(() => {
-                var result = Object.assign(new CityWeatherDetails(), 
-                    mockCityData[Object.keys(mockCityData).filter(k => mockCityData[k].id === cityID).shift()]);
-                if (result.main) {
-                    result.main.temp += (Math.random() >= 0.5 ? 1 : -1) * Math.round(Math.random() * 5);
-                }
-                result.setTemperatures();
+                var result = mockCityData[Object.keys(mockCityData).filter(k => mockCityData[k].id === cityID).shift()];
+                result = WeatherService._prepareResult(result);
                 resolve(result);
             }, 700);
         });
@@ -40,11 +49,8 @@ export class WeatherService {
         console.log(`Mock request: \${BASE_URL}weather?lat=${lat}&lon=${lon}&appid=\${appid}`);
         return new Promise(resolve => {
             setTimeout(() => {
-                var result = Object.assign(new CityWeatherDetails(), mockCityData[Object.keys(mockCityData)[0]]);
-                if (result.main) {
-                    result.main.temp += (Math.random() >= 0.5 ? 1 : -1) * Math.round(Math.random() * 5);
-                }
-                result.setTemperatures();
+                var result = mockCityData[Object.keys(mockCityData)[0]];
+                result = WeatherService._prepareResult(result);
                 resolve(result);
             }, 700);
         });
